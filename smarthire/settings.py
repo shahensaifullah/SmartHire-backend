c4b16c140979f30a8985c42fb4866da6376e0213
shahensaifullah <shahriar350@gmail.com>
2026-04-13 22:26:48 +0200

    added docker-compose

--- smarthire/settings.py
+++ smarthire/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'smarthire.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "smart_hire",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
